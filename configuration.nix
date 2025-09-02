b5e7d21cf932905585fba1e7f87940a3e8588953
Moritz Nachförg <moritz@nachfoerg.at>
2025-09-02 10:45:32 +0200

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+
+  networking.hostName = "HTPC";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Vienna";
+
+  i18n.defaultLocale = "de_AT.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "daily";
+  nix.gc.options = "--delete-older-than 5d";
+
+  services.xserver.xkb = {
+    layout = "at";
+    variant = "nodeadkeys";
+  };
+
+  services.xserver.enable = true;
+  services.displayManager.autoLogin.user = "kodi";
+  services.xserver.displayManager.lightdm.greeter.enable = false;
+  services.xserver.desktopManager.kodi = {
+    enable = true;
+    addons = with pkgs.kodiPackages; [
+      youtube
+      jellyfin
+      netflix
+      orftvthek
+      joystick
+      sponsorblock
+      steam-launcher
+      bluetooth-manager
+      libretro
+    ];
+  };
+
+  # Define a user account
+  users.extraUsers.kodi.isNormalUser = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+  hardware.xone.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    libcec
+    git
+    helix
+  ];
+
+  system.stateVersion = "25.05";
+}
